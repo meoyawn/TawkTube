1adb933d76fd6a4d2d6ed1c431530c8a27238434
adelnizamutdinov <stiggpwnz@gmail.com>
2017-07-30 13:06:04 +0300

    cleaner errors

--- src/main/kotlin/adeln/main.kt
+++ src/main/kotlin/adeln/main.kt
@@ -1,15 +1,15 @@
 package adeln
 
-import com.google.api.services.youtube.YouTube
 import com.rometools.rome.io.SyndFeedOutput
 import okhttp3.OkHttpClient
 import org.jetbrains.ktor.content.respondWrite
 import org.jetbrains.ktor.host.embeddedServer
+import org.jetbrains.ktor.http.HttpStatusCode
 import org.jetbrains.ktor.netty.Netty
-import org.jetbrains.ktor.pipeline.PipelineContext
 import org.jetbrains.ktor.response.respondRedirect
 import org.jetbrains.ktor.routing.get
 import org.jetbrains.ktor.routing.routing
+import java.io.PrintWriter
 
 object Secrets {
     val YT_KEY = "AIzaSyBXaU6RB0KwBFqEz5sdcyjXiNySefvUHLc"
@@ -33,7 +33,22 @@ fun main(args: Array<String>) {
                 val videoId = VideoId(call.parameters["v"]!!)
             }
 
-            get("/playlist") { playlist(client, youtube) }
+            get("/playlist") {
+                try {
+                    val id = call.parameters["list"]
+
+                    val feed = asFeed(client, youtube, PlaylistId(id!!))
+
+                    call.respondWrite {
+                        SyndFeedOutput().output(feed, this)
+                    }
+                } catch (e: Exception) {
+                    call.response.status(HttpStatusCode.NotImplemented)
+                    call.respondWrite {
+                        e.printStackTrace(PrintWriter(this))
+                    }
+                }
+            }
 
             get("/audio") {
                 val videoId = VideoId(call.parameters["v"]!!)
@@ -44,16 +59,3 @@ fun main(args: Array<String>) {
         }
     }.start(wait = true)
 }
-
-suspend fun PipelineContext<*>.playlist(client: OkHttpClient, youTube: YouTube): Unit =
-    try {
-        val id = call.parameters["list"]
-
-        val feed = asFeed(client, youTube, PlaylistId(id!!))
-
-        call.respondWrite {
-            SyndFeedOutput().output(feed, this)
-        }
-    } catch (e: Exception) {
-        e.printStackTrace()
-    }
