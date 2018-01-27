2173b70154cc39275dcec9874e23fec7754d5d43
adelnizamutdinov <stiggpwnz@gmail.com>
2018-01-27 12:59:01 +0300

    revert redirect audio

--- src/main/kotlin/adeln/main.kt
+++ src/main/kotlin/adeln/main.kt
@@ -6,17 +6,11 @@ import kotlinx.html.head
 import kotlinx.html.input
 import okhttp3.HttpUrl
 import okhttp3.OkHttpClient
-import okhttp3.Request
-import okio.Okio
-import org.jetbrains.ktor.cio.WriteChannel
-import org.jetbrains.ktor.cio.toOutputStream
-import org.jetbrains.ktor.content.FinalContent
 import org.jetbrains.ktor.host.embeddedServer
 import org.jetbrains.ktor.html.respondHtml
 import org.jetbrains.ktor.http.HttpHeaders
 import org.jetbrains.ktor.http.HttpStatusCode
 import org.jetbrains.ktor.netty.Netty
-import org.jetbrains.ktor.response.respond
 import org.jetbrains.ktor.response.respondRedirect
 import org.jetbrains.ktor.response.respondText
 import org.jetbrains.ktor.response.respondWrite
@@ -103,22 +97,7 @@ fun main(args: Array<String>) {
 
                 val audio = audio(client, videoId, player)
 
-                val req = Request.Builder()
-                    .url(audio.url)
-                    .build()
-
-                val resp = client.newCall(req).await()
-                call.response.status(HttpStatusCode.fromValue(resp.code()))
-                call.response.okHeaders = resp.headers()
-                call.respond(object : FinalContent.WriteChannelContent() {
-                    suspend override fun writeTo(channel: WriteChannel) {
-                        Okio.buffer(Okio.sink(channel.toOutputStream())).use { sink ->
-                            resp.body()!!.source().use { src ->
-                                src.readAll(sink)
-                            }
-                        }
-                    }
-                })
+                call.respondRedirect(audio.url.toString())
             }
 
             route("/yandexdisk") {
