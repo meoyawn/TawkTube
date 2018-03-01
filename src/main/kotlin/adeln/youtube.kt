5cf5576a676b049ccceb041cdfa2dc22495d351f
adelnizamutdinov <stiggpwnz@gmail.com>
2018-03-01 16:56:21 +0300

    limit 23

--- src/main/kotlin/adeln/youtube.kt
+++ src/main/kotlin/adeln/youtube.kt
@@ -69,7 +69,7 @@ fun playlistEntries(yt: YouTube, playlistID: PlaylistID, player: Player): List<S
     val full = paging<PlaylistVideos, String>(
         load = { yt.playlistVideos(playlistID, pageToken = it) },
         nextPage = { it.items.nextPageToken },
-        limit = 24
+        limit = 23
     )
 
     val items = full.flatMap { it.items.items }
