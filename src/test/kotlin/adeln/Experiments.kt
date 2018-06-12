4f3af2057df529ba246dadd0d5db283233f60642
adelnizamutdinov <stiggpwnz@gmail.com>
2018-06-12 16:35:47 +0300

    bump deps

--- src/test/kotlin/adeln/Experiments.kt
+++ src/test/kotlin/adeln/Experiments.kt
@@ -35,6 +35,6 @@ class Experiments {
 
     @Test
     fun instantRegret() {
-        playlistEntries(mkYoutube(), PlaylistID("PLiQrdzH3aBWi6nh1kdbYfy2dd1CSOwBz5"), Player.BROWSER) should haveSize(74)
+        playlistEntries(mkYoutube(), PlaylistID("PLiQrdzH3aBWi6nh1kdbYfy2dd1CSOwBz5"), Player.BROWSER) should haveSize(72)
     }
 }
