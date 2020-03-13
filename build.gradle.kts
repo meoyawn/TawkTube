c2158b81358a8f2393bb8b38b4c9327f9ac3b260
adelnizamutdinov <stiggpwnz@gmail.com>
2020-03-13 13:48:36 +0300

    draft

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 }
 
 dependencies {
-    listOf(
+    sequenceOf(
         "org.jetbrains.kotlin:kotlin-stdlib-jdk8",
         "org.jetbrains.kotlin:kotlin-reflect",
         "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.4"
@@ -23,18 +23,20 @@ dependencies {
 
     implementation("com.squareup.okhttp3:okhttp:4.4.1")
 
-    listOf("rome", "rome-modules").map { "com.rometools:$it:1.12.2" }
+    sequenceOf("rome", "rome-modules")
+        .map { "com.rometools:$it:1.12.2" }
         .forEach(::implementation)
 
     implementation("com.google.apis:google-api-services-youtube:v3-rev20180511-1.27.0")
     implementation("com.google.api-client:google-api-client-jackson2:1.27.0")
 
-    listOf("ktor-server-netty", "ktor-html-builder", "ktor-client-apache").map { "io.ktor:$it:1.3.2" }
+    sequenceOf("ktor-server-netty", "ktor-html-builder", "ktor-client-apache")
+        .map { "io.ktor:$it:1.3.2" }
         .forEach(::implementation)
 
     implementation("com.yandex.android:disk-restapi-sdk:1.03")
 
-    listOf("org.jetbrains.kotlin:kotlin-test-junit5","org.junit.jupiter:junit-jupiter-engine:5.6.0")
+    sequenceOf("org.jetbrains.kotlin:kotlin-test-junit5", "org.junit.jupiter:junit-jupiter-engine:5.6.0")
         .forEach(::testImplementation)
 }
 
